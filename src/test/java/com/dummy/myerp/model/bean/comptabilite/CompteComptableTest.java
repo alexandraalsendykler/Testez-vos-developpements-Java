a83d9e5632ce37cc2c2898cda360136fee65aac2
Alexandra <alexandra.alsen.dykler@gmail.com>
2021-07-15 02:40:37 +0200

    4ème test

--- src/test/java/com/dummy/myerp/model/bean/comptabilite/CompteComptableTest.java
+++ src/test/java/com/dummy/myerp/model/bean/comptabilite/CompteComptableTest.java
@@ -1,5 +1,22 @@
 package com.dummy.myerp.model.bean.comptabilite;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
 public class CompteComptableTest {
 
+@Test
+public void CompteComptable() {
+CompteComptable pList;
+CompteComptable pList2;
+pList = new CompteComptable(1,"test");
+pList2 = new CompteComptable(2,"test");
+List<CompteComptable> plists = new ArrayList<CompteComptable>();
+plists.add(pList);
+plists.add(pList2);
+Assert.assertEquals(pList.toString(), pList, CompteComptable.getByNumero(plists, 1));
+}
 }
